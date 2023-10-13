45d144071667f6e580da3fdf363c15e29a16b5fc
RB Projects <r.b.batinov@gmail.com>
2023-10-13 19:12:05 +0300

    - custom css and js files

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,12 @@
-require('./bootstrap');
+const navbar = document.getElementsByTagName('nav')[0];
+let top = navbar.offsetTop;
+function stickynavbar() {
+  if (window.scrollY >= top) {    
+    navbar.classList.add('sticky');
+  } else {
+    navbar.classList.remove('sticky');    
+  }
+}
+window.addEventListener('scroll', stickynavbar);
+
+
