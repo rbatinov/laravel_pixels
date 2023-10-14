36b698adf45202ffa785d8e58ffcb160396a3da8
RB Projects <r.b.batinov@gmail.com>
2023-10-14 07:51:12 +0300

    - add admin delete button to single user  page

--- resources/views/users/user.blade.php
+++ resources/views/users/user.blade.php
@@ -1,6 +1,16 @@
 <x-layout>
     <x-page-header-value :value="__('labels.l_user_profile_heading') . $user->name"></x-page-header-value> 
 
+    <div class="text-center">
+        @can('Admin')
+            <form method="post" action="{{ route('admin_dashboard_destroy_user_url', ['userInfo' => $user->id]) }}">
+                @csrf
+                @method('delete')
+                <button class="text-dash-red-500 btn-gradient mt-4 mb-4"><i class="fa-solid fa-trash"></i>{{ __('labels.l_delete') }}</button>
+            </form>
+        @endcan
+    </div>
+
     <x-grid-picture>
         @unless (count($userImages) == 0)
             @foreach ($userImages as $key => $user_image)
